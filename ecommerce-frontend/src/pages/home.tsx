7239a154dfa062a0f4e23195d30271e04429f189
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-23 20:01:21 +0530

    product-card

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
+
 const Home = () => {
-  return <div>home</div>;
+  const addToCartHandler = () => {};
+
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="000001"
+          name="Macbook"
+          price={120000}
+          photo="https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg"
+          stock={100}
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
