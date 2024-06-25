26467250c7a5bf55d98079e3bcbc7f02b3e56b9d
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-25 19:39:23 +0530

    ErrorHandler

--- ecommerce-backend/dist/app.js
+++ ecommerce-backend/dist/app.js
@@ -2,6 +2,7 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
 const port = 4000;
 connectDB();
 const app = express();
@@ -11,6 +12,7 @@ app.get("/", (req, res) => {
 });
 // Using Routes
 app.use("/api/v1/user", userRoute);
+app.use(errorMiddleware);
 app.listen(port, () => {
     console.log(`Express is working on http://localhost:${port}`);
 });
--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 // Importing Routes
 import userRoute from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const port = 4000;
 
@@ -18,6 +19,8 @@ app.get("/", (req, res) => {
 
 // Using Routes
 app.use("/api/v1/user", userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Express is working on http://localhost:${port}`);
