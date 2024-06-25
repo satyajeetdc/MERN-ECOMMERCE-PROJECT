3cd57cf803ac123e1e58270a5e2488d0a284095a
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-25 17:45:37 +0530

    new user

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -2,10 +2,19 @@ import express from "express";
 
 // Importing Routes
 import userRoute from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
 
 const port = 4000;
 
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Working with /api/v1");
+});
 
 // Using Routes
 app.use("/api/v1/user", userRoute);
