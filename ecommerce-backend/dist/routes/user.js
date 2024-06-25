06338faa3c78939d39518597f08ef2b058006923
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-25 21:15:16 +0530

    commit

--- ecommerce-backend/dist/routes/user.js
+++ ecommerce-backend/dist/routes/user.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import { getAllUsers, getUser, newUser } from "../controllers/user.js";
 const app = express.Router();
 // route -> /api/v1/user/new
 app.post("/new", newUser);
+// route -> /api/v1/user/all
+app.get("/all", getAllUsers);
+// route -> /api/v1/user/dynamicID
+app.get("/:id", getUser);
 export default app;
--- ecommerce-backend/src/controllers/user.ts
+++ ecommerce-backend/src/controllers/user.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
 import { TryCatch } from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
 
 export const newUser = TryCatch(
   async (
@@ -11,7 +12,20 @@ export const newUser = TryCatch(
   ) => {
     const { name, email, photo, gender, _id, dob } = req.body;
 
-    const user = await User.create({
+    let user = await User.findById(_id);
+
+    if (user)
+      return res.status(200).json({
+        success: true,
+        message: `Welcome ${user.name}`,
+      });
+
+    if (!_id || !name || !email || !photo || !gender || !dob)
+      return next(
+        new ErrorHandler("Please enter all the required fields", 400)
+      );
+
+    user = await User.create({
       name,
       email,
       photo,
@@ -26,3 +40,24 @@ export const newUser = TryCatch(
     });
   }
 );
+
+export const getAllUsers = TryCatch(async (req, res, next) => {
+  const users = await User.find({});
+
+  return res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+export const getUser = TryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+
+  if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+  return res.status(200).json({
+    success: true,
+    user,
+  });
+});
