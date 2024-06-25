04e5efcc1772f3c4d6e514d146c878bd28158d90
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-25 13:16:11 +0530

    user

--- ecommerce-backend/dist/app.js
+++ ecommerce-backend/dist/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+// Importing Routes
+import userRoute from "./routes/user.js";
 const port = 4000;
 const app = express();
+// Using Routes
+app.use("/api/v1/user", userRoute);
 app.listen(port, () => {
     console.log(`Express is working on http://localhost:${port}`);
 });
--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 
+// Importing Routes
+import userRoute from "./routes/user.js";
+
 const port = 4000;
 
 const app = express();
+
+// Using Routes
+app.use("/api/v1/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Express is working on http://localhost:${port}`);
