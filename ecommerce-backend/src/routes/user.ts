b6a187ddf848958263cb72fd396f083f7992aa82
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-06-26 09:24:43 +0530

    multer

--- ecommerce-backend/src/routes/user.ts
+++ ecommerce-backend/src/routes/user.ts
@@ -5,6 +5,7 @@ import {
   getUser,
   newUser,
 } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
@@ -12,9 +13,9 @@ const app = express.Router();
 app.post("/new", newUser);
 
 // route -> /api/v1/user/all
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 
 // route -> /api/v1/user/dynamicID
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 
 export default app;
