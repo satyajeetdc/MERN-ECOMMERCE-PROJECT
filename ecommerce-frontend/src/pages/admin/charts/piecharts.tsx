824797f6e55bf556539ea39a3517dded4f284fc8
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-07-05 10:49:41 +0530

    Project v1 commit

--- ecommerce-frontend/src/pages/admin/charts/piecharts.tsx
+++ ecommerce-frontend/src/pages/admin/charts/piecharts.tsx
@@ -2,15 +2,14 @@ import AdminSidebar from "../../../components/admin/AdminSidebar";
 import { DoughnutChart, PieChart } from "../../../components/admin/Charts";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
-import { CustomError } from "../../../types/api-types";
-import toast from "react-hot-toast";
 import { usePieQuery } from "../../../redux/api/dashboardAPI";
 import { SkeletonLoader } from "../../../components/loader";
+import { Navigate } from "react-router-dom";
 
 const PieCharts = () => {
   const { user } = useSelector((state: RootState) => state.userReducer);
 
-  const { isLoading, data, error, isError } = usePieQuery(user?._id!);
+  const { isLoading, data, isError } = usePieQuery(user?._id!);
 
   const order = data?.charts.orderFullfillment!;
   const categories = data?.charts.productCategories!;
@@ -19,10 +18,7 @@ const PieCharts = () => {
   const ageGroup = data?.charts.usersAgeGroup!;
   const adminCustomer = data?.charts.adminCustomer!;
 
-  if (isError) {
-    const err = error as CustomError;
-    toast.error(err.data.message);
-  }
+  if (isError) return <Navigate to={"/admin/dashboard"} />;
 
   return (
     <div className="admin-container">
